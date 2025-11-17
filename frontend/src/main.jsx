cb2253e3de7b78ec194e70f451d30638a9655e2a
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-17 05:55:19 -0500

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+
+// Import global styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Import the main App component
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* The main application component */}
+    <App />
+    
+    {/* Toaster is the component that will render all notifications */}
+    <Toaster
+      position="bottom-right"
+      toastOptions={{
+        duration: 5000,
+        style: {
+          background: '#363636',
+          color: '#fff',
+        },
+      }}
+    />
+  </React.StrictMode>
+);```
+
+---
+
+#### **Next Steps:**
+
+1.  Save this file in the `/frontend/src` directory.
+
+Your `src` folder should now contain this single file:
