1adbce906d882c6eba2ae81b8dab3de804e71e23
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-17 06:00:38 -0500

    Create ReportsPage.jsx

---
+++ frontend/src/pages/ReportsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ReportsPage() {
+  return (
+    <div>
+      <h2>Universal Report Builder</h2>
+      <p>Report builder functionality will be built here.</p>
+    </div>
+  );
+}
+
+export default ReportsPage;
