f0b284ec940770bc34a8e876e735997dde012f71
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-17 05:58:54 -0500

    Create AdminPage.jsx

---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AdminPage() {
+  return (
+    <div>
+      <h2>Admin Console</h2>
+      <p>Admin console functionality will be built here.</p>
+    </div>
+  );
+}
+
+export default AdminPage;
