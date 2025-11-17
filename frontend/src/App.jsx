056e7bfde0feef1c723a67c9896aa2367f419bd9
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-17 05:56:06 -0500

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Import Page Components (we will create these next)
+import DashboardPage from './pages/DashboardPage';
+import AdminPage from './pages/AdminPage';
+import ReportsPage from './pages/ReportsPage';
+
+// Import Layout Component (we will create this next)
+import MainLayout from './components/layout/MainLayout';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* All pages will share the MainLayout component */}
+        <Route path="/" element={<MainLayout />}>
+          {/* The default page to render at "/" */}
+          <Route index element={<DashboardPage />} />
+          
+          {/* The page to render at "/admin" */}
+          <Route path="admin" element={<AdminPage />} />
+          
+          {/* The page to render at "/reports" */}
+          <Route path="reports" element={<ReportsPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
