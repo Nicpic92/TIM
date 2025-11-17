45cb8f2dd93597bc26fa0c04729cf49f994372b5
Nicpic92 <nickolaspicardircm@gmail.com>
2025-11-17 05:58:35 -0500

    Create DashboardPage.jsx

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Dashboard functionality will be built here.</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
